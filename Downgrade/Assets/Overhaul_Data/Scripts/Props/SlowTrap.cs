fff5ca26c13c7ad3f09c007a7ead891afce31d4f
Juan Pablo Arredondo Araneda <j.arredondoaraneda@uandresbello.edu>
2024-05-28 02:34:59 -0400

    SlowWorking
    Si matas dentro del slow da error (ver)
    
    Hacer que se dropee y desaparezca?

--- Downgrade/Assets/Overhaul_Data/Scripts/Props/SlowTrap.cs
+++ Downgrade/Assets/Overhaul_Data/Scripts/Props/SlowTrap.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class SlowTrap : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private float radius;
+    [SerializeField] private float multiplier = 0.7f;
+    [SerializeField] private Dictionary<GameObject, float> originalSpeeds = new Dictionary<GameObject, float>(); 
     void Start()
     {
         
@@ -13,15 +16,72 @@ public class SlowTrap : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Physics.OverlapBox(transform.position, transform.localScale, Quaternion.identity);
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider hit in hitColliders)
+        {
+            if (hit.CompareTag("Player") || hit.CompareTag("Enemy"))
+            {
+                float speedToSave = 1;
+                if (!originalSpeeds.ContainsKey(hit.gameObject))
+                {
+
+                   
+                    
+                    if (hit.CompareTag("Player"))
+                    {
+                        speedToSave = hit.GetComponent<PlayerControllerOverhaul>().GetSpeed();
+                        hit.GetComponent<PlayerControllerOverhaul>().SetSpeed(speedToSave * multiplier);
+                    }
+
+                    if (hit.CompareTag("Enemy"))
+                    {
+                        speedToSave = hit.GetComponent<EnemyBase>().speed;
+                        hit.GetComponent<EnemyBase>().speed = speedToSave * multiplier;
+                    }
+                    originalSpeeds[hit.gameObject] = speedToSave;
+                }
+                
+
+                
+            }
+        }
+
+        // Restore speed for objects that have left the sphere
+        List<GameObject> toRemove = new List<GameObject>();
+        foreach (var obj in originalSpeeds)
+        {
+            bool isStillInside = false;
+            foreach (var hit in hitColliders)
+            {
+                if (hit.gameObject == obj.Key)
+                {
+                    isStillInside = true;
+                    break;
+                }
+            }
+            if (!isStillInside)
+            {
+                if (obj.Key.CompareTag("Player"))
+                {
+                    obj.Key.GetComponent<PlayerControllerOverhaul>().SetSpeed(originalSpeeds[obj.Key]);
+                }
+                else if (obj.Key.CompareTag("Enemy"))
+                {
+                    obj.Key.GetComponent<EnemyBase>().speed = originalSpeeds[obj.Key];
+                }
+                toRemove.Add(obj.Key);
+            }
+        }
+
+        // Remove restored objects from the dictionary
+        foreach (var obj in toRemove)
+        {
+            originalSpeeds.Remove(obj);
+        }
     }
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
+   
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireCube(transform.position, transform.localScale);
+        Gizmos.DrawWireSphere(transform.position,radius);
     }
 }
